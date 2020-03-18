dd376cfebc0a67fb2f33bee4d9a1ad68381849c5
CammiloB <sergio.camilo.blanco@gmail.com>
2020-03-17 20:38:32 -0500

    Login Integration

---
+++ lib/login.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:ejemplo_construccion/widget/button.dart';
+import 'package:ejemplo_construccion/widget/first.dart';
+import 'package:ejemplo_construccion/widget/forgot.dart';
+import 'package:ejemplo_construccion/widget/inputEmail.dart';
+import 'package:ejemplo_construccion/widget/password.dart';
+import 'package:ejemplo_construccion/widget/textLogin.dart';
+import 'package:ejemplo_construccion/widget/verticalText.dart';
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.black87, Colors.blueGrey]),
+        ),
+        child: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  VerticalText(),
+                  TextLogin(),
+                ]),
+                InputEmail(),
+                PasswordInput(),
+                ButtonLogin(),
+                FirstTime(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,222 @@
-import 'package:ejemplo_construccion/pages/calls.dart';
-import 'package:ejemplo_construccion/pages/chats.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ejemplo_construccion/delayed_animation.dart';
+import 'package:ejemplo_construccion/login.dart';
 
 
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          primaryColor: Colors.cyan[900],
-          primaryColorDark: Colors.cyan[900],
-          accentColor: Colors.cyan[900]),
-      home: MyHomePage(title: 'PhotoBoard'),
-    );
-  }
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(MaterialApp(
+    title: "PhotoBoard",
+    home: MyApp()
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class MyApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-  int _counter = 0;
-  int _currentPage = 0;
-  
-  TabController _tabController;
+
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  final int delayedAmount = 500;
+  double _scale;
+  AnimationController _controller;
   @override
   void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 200,
+      ),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    )..addListener(() {
+        setState(() {});
+      });
     super.initState();
-    _tabController = TabController(vsync: this, length: 2,initialIndex: 0);
-  }
-
- 
-  void _changePage(int index) {
-    setState(() {
-      _currentPage = index;
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        elevation: 0.5,
-        bottom: TabBar(
-          indicatorColor: Colors.white,
-          controller: _tabController,
-          tabs: <Widget>[
-          Tab(text: 'Calendario'),
-          Tab(text: 'Materias',)
-        ],),
-      ),
-      /*
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _changePage,
-        currentIndex: _currentPage,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.fast_rewind), title: Text('Retroceder')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.extension), title: Text('Funcionalidades')),
-          BottomNavigationBarItem( 
-              icon: Icon(Icons.face), title: Text('Usuarios')),
-        ],
-      ),*/
-      body: TabBarView(
-        controller: _tabController,
-        children: <Widget>[CallPage(), ChatPage()],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.camera, color: Colors.white),
-       
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    final color = Colors.white;
+    _scale = 1 - _controller.value;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.black87, Colors.blueGrey]),
+            ),
+            child: ListView(
+              children: <Widget>[
+                Center(
+                child:Column(
+                children: <Widget>[
+                 AvatarGlow(
+                  endRadius: 90,
+                  duration: Duration(seconds: 2),
+                  glowColor: Colors.white24,
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 2),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 8.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: FlutterLogo(
+                          size: 50.0,
+                        ),
+                        radius: 50.0,
+                      )),
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Bienvenido a",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 1000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "PhotoBoard",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 2000,
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                SizedBox(
+                  height: 100.0,
+                ),
+                DelayedAnimation(
+                child: GestureDetector(
+                  onTapDown: _onTapDown,
+                  onTapUp: _onTapUp,
+                  child: Transform.scale(
+                    scale: _scale,
+                    child: _loginButtonUI,
+                  ),
+                ),
+                delay: delayedAmount + 4000,
+              ),
+              SizedBox(height: 50.0,),
+                DelayedAnimation(
+                child: GestureDetector(
+                  onTapDown: _onTapDown,
+                  onTapUp: _onTapUp,
+                  child: Transform.scale(
+                    scale: _scale,
+                    child: _registerButtonUI,
+                  ),
+                ),
+                delay: delayedAmount + 4000,
+              ),
+                ]
+                ),
+                )
+              ],
+            ),
+          )
+          ),
     );
+  }
+
+  Widget get _loginButtonUI => Container(
+        height: 60,
+        width: 270,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue[300],
+              blurRadius: 10.0, // has the effect of softening the shadow
+              spreadRadius: 1.0, // has the effect of extending the shadow
+              offset: Offset(
+                5.0, // horizontal, move right 10
+                5.0, // vertical, move down 10
+              ),
+            ),
+          ],
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) => LoginPage()
+              )
+            );
+          },
+          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+          child: const Text(
+            'Ingresar',
+            style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                ),
+          ),
+        ),
+      );
+
+    Widget get _registerButtonUI => Container(
+        height: 60,
+        width: 270,
+         decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue[300],
+              blurRadius: 10.0, // has the effect of softening the shadow
+              spreadRadius: 1.0, // has the effect of extending the shadow
+              offset: Offset(
+                5.0, // horizontal, move right 10
+                5.0, // vertical, move down 10
+              ),
+            ),
+          ],
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) => LoginPage()
+              )
+            );
+          },
+          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+          child: const Text(
+            'Registrarse',
+            style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                ),
+          ),
+        ),
+      );
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
   }
 }
